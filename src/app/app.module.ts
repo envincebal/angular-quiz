6243a3f3c9333f21364ae5949a9fb349e40a5059
envincebal <vincentyan1@gmail.com>
2019-12-10 03:09:00 -0800

    git commit -m "first commit"

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { } 
