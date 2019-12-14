d88c2900b8099426303067e25092b704b96c9dda
envincebal <vincentyan1@gmail.com>
2019-12-14 01:37:11 -0800

    Added routes and styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    MatCardModule,
+    BrowserAnimationsModule,
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule
