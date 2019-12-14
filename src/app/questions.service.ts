2814fd8ec5ff7898043d589e1755d46db5202abb
envincebal <vincentyan1@gmail.com>
2019-12-14 04:41:29 -0800

    Update questions service.

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -11,7 +11,7 @@ export class QuestionsService {
   constructor(private http: HttpClient) {}
 
   public getQuizzes(){
-    return this.http.get(`../assets/quiz-list.json`).pipe(
+    return this.http.get(`./assets/quiz-list.json`).pipe(
       map((results: any[]) => {
         return results.map(r => new Quiz(r.label, r.name, r.description, r.fileName));
       })
@@ -19,7 +19,7 @@ export class QuestionsService {
   }
 
   public getQuestions(fileName: string){
-    return this.http.get(`../assets/${fileName}.json`).pipe(
+    return this.http.get(`./assets/${fileName}.json`).pipe(
       map((results: any[]) => {
         return results.map(r => new Question (r.label, r.choices));
       })
